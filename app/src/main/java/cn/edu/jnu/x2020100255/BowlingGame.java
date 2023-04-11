2f348a6fdf12bf289238dc1d619bb2c464443699
hhj <1229648668@qq.com>
2023-04-11 11:25:00 +0800

    forth commit

--- app/src/main/java/cn/edu/jnu/x2020100255/BowlingGame.java
+++ app/src/main/java/cn/edu/jnu/x2020100255/BowlingGame.java
@@ -1,12 +1,14 @@
 package cn.edu.jnu.x2020100255;
 
 public class BowlingGame {
+    int score=0;
 
     public void roll(int pin) {
+        score+=pin;
 
     }
 
     public int score() {
-        return 0;
+        return score;
     }
 }
