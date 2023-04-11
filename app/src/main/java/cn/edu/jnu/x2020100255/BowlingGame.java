27026c72aad78e71152032a8a32464e5cbddd2af
hhj <1229648668@qq.com>
2023-04-11 11:44:43 +0800

    sixth commit

--- app/src/main/java/cn/edu/jnu/x2020100255/BowlingGame.java
+++ app/src/main/java/cn/edu/jnu/x2020100255/BowlingGame.java
@@ -1,14 +1,26 @@
 package cn.edu.jnu.x2020100255;
 
 public class BowlingGame {
-    int score=0;
+    int pins[]=new int[21];
+    int currentPinIndex=0;
 
     public void roll(int pin) {
-        score+=pin;
-
+        pins[currentPinIndex++]=pin;
     }
 
     public int score() {
-        return score;
+        int totalScore=0;
+        for(int scoreIndex=0;scoreIndex<pins.length;scoreIndex++)
+        {
+            totalScore+=pins[scoreIndex];
+            //if is a spare ,will add next score and its next score into current score
+            if(scoreIndex<19)
+            {
+                if(10==pins[scoreIndex]+pins[scoreIndex+1]) {
+                    totalScore+=pins[scoreIndex+2];
+                }
+            }
+        }
+        return totalScore;
     }
 }
