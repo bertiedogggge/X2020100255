886bf5777cec45eb73af4a4ae9bdf285cd63836f
hhj <1229648668@qq.com>
2023-04-11 13:12:51 +0800

    eleventh commit

--- app/src/main/java/cn/edu/jnu/x2020100255/MainActivity.java
+++ app/src/main/java/cn/edu/jnu/x2020100255/MainActivity.java
@@ -3,6 +3,10 @@ package cn.edu.jnu.x2020100255;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +14,21 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Button buttonCalculate=this.findViewById(R.id.button2);
+        EditText editTextScore=this.findViewById(R.id.searchView);
+        TextView textViewScore=this.findViewById(R.id.textView2);
+        buttonCalculate.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String scores=editTextScore.getText().toString();
+                String []arrayScore= scores.split(",");
+                BowlingGame game=new BowlingGame();
+                for(int i=0;i<arrayScore.length;i++){
+                    game.roll(Integer.parseInt(arrayScore[i]));
+                }
+                textViewScore.setText("Total score is:"+game.score());
+            }
+
+        });
     }
 }
